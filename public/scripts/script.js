8d6bf7557ed8e448f710581bb2d61159a22a66d6
adamgothmann <adamgothmann@gmail.com>
2017-06-07 19:30:05 -0500

    finished routing

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,15 +1,26 @@
 angular.module('statsApp', ['ngRoute'])
   .config(["$routeProvider", function($routeProvider){
-    $routeProfider.
-      when("/home", {
-        templateUrl: "views/home.html",
+    $routeProvider.
+      when('/home', {
+        templateUrl: "/views/home.html",
         controller: "mainController"
       }).
       otherwise({
         redirectTo: "/home"
       });
   }])
+  .controller('mainController', ['$scope', '$http', function($scope, $http){
+    console.log('something');
+    $scope.addTeam = function(){
+      console.log($scope.teamName);
+      var teamToSend = {
+        teamName: $scope.teamName
+      };
 
-  .controller('mainController', function($scope, $http) {
-    console.log('controller');
-  });
+      $http({
+        method: 'POST',
+        url: '/addTeam',
+        data: teamToSend
+      });
+    };
+  }]);
